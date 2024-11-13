d7326026091d431ef8fcc58d104d3280e8f60bb3
Clarence Jordan <clarencejordan087@gamil.com.com>
2024-11-13 09:48:16 -0600

    add link (no direction yet)

--- Frontend/website/my-app/src/components/sidebar/Navbar.tsx
+++ Frontend/website/my-app/src/components/sidebar/Navbar.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import {navBarMetaData} from "@/constraints/constraints"
+import NavLinks from './NavLinks'
 // TODO iterate though navbar send data as props
 const Navbar = () => {
   return (
-    <div className='bg-white w-52 h-[85vh] rounded-xl shadow-xl'>
-        
+    <div className='bg-white w-60 h-[85vh] rounded-xl shadow-xl'>
+      <div className='flex justify-center mt-10 '>
+      <div className='flex flex-col justify-center gap-4'>
+       {
+          navBarMetaData.map((data,i)=><NavLinks key={i} name={data.name} src={data.src} alt={data.alt} href={data.href}/>)
+        }
+       </div>
+      </div>
     </div>
   )
 }
